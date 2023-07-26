3f7ed7f195789b33185eb5fbb2346cdddc51a10a
Christian Beutel <>
2023-07-26 16:59:42 +0200

    switches to pocketbase stripe

--- store/stripe.ts
+++ store/stripe.ts
@@ -1,6 +1,6 @@
 import { Action, Module, Mutation, MutationAction, VuexModule } from 'vuex-module-decorators';
 import { StripeInvoice } from '~/models/stripe';
-import { $axios, stripeStore } from '~/utils/store-accessor';
+import { $axios, $pb, stripeStore } from '~/utils/store-accessor';
 
 @Module({
     name: 'stripe',
@@ -14,8 +14,9 @@ export default class StripeStore extends VuexModule {
     @Action
     async createSession(data: { price_id: string, email: string, user_id: string }): Promise<string | null> {
         try {
-            const response = await $axios.post('/stripe/session', data)
-            return response.data?.data;
+            const response = await $pb.send('/api/stripe/session', { body: data, method: 'post' })
+
+            return response.data;
         } catch {
             return null;
         }
@@ -24,10 +25,10 @@ export default class StripeStore extends VuexModule {
     @MutationAction({ mutate: ['invoices'] })
     async listInvoices(customerId: string) {
         try {
-            const response = await $axios.get('/stripe/invoice', { params: { customer_id: customerId } })
-            return {invoices: response.data?.data};
+            const response = await $pb.send('/api/stripe/invoice', { params: { customer_id: customerId }, })
+            return { invoices: response.data };
         } catch {
-            return {invoices: stripeStore.invoices}
+            return { invoices: stripeStore.invoices }
         }
     }
 }
